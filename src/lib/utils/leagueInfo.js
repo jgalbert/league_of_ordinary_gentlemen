77801ff0bcf8dccd2819358ab088788278f7d4ff
jgalbert <113197453+jgalbert@users.noreply.github.com>
2024-01-12 09:25:34 -0500

    Update leagueID for 2024

--- src/lib/utils/leagueInfo.js
+++ src/lib/utils/leagueInfo.js
@@ -1,5 +1,5 @@
 /*   STEP 1   */
-export const leagueID = "916476233674907648"; // your league ID
+export const leagueID = "1049388190487986176"; // your league ID
 export const leagueName = "Leauge of Ordinary Gentlemen"; // your league name
 export const dues = 50; // (optional) used in template constitution page
 export const dynasty = true; // true for dynasty leagues, false for redraft and keeper
